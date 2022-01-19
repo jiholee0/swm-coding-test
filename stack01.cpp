5c144d5df9aa156fb99ea3d036dfe5b2c10a1e21
Lee jiho <42367169+jiholee0@users.noreply.github.com>
2022-01-20 01:17:55 +0900

    Add files via upload

---
+++ stack01.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    stack<int> day;
+
+    int size = progresses.size();
+    int restDay, x, now;
+
+    for (int i = size - 1; i >= 0; i--) {
+        restDay = (100 - progresses[i]) / speeds[i];
+        if (((100 - progresses[i]) % speeds[i]) != 0) (restDay)++;
+        day.push(restDay);
+    }
+
+    while (!day.empty()) {
+        now = day.top();
+        x = 0;
+        day.pop();
+        x++;
+        if (day.empty()) {  // 抗寇贸府
+            answer.push_back(x);
+            break;
+        }
+        while (day.top() - now <= 0) {
+            day.pop();
+            x++;
+            if (day.empty()) { // 抗寇贸府
+                break;
+            }
+        }
+        answer.push_back(x);
+    }
+
+    return answer;
+}
